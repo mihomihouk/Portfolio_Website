e1488aa6850368a76007e8ee2cea3452a7643a94
mihomihouk <mihomihouk@gmail.com>
2022-05-21 11:09:02 +0100

    improved UI in about page

--- pages/about-me.js
+++ pages/about-me.js
@@ -6,15 +6,11 @@ import {
   Button,
   Text,
   Link,
-  List,
-  ListItem,
-  ListIcon,
   Heading
 } from '@chakra-ui/react'
-import { MdCheckCircle } from 'react-icons/md'
 import Layout from '../components/layouts/article'
 import SubHeading from '../components/subHeading'
-import { BioSection, BioYear } from '../components/bio'
+
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 
@@ -28,78 +24,56 @@ const Profile = () => {
           borderWidth="1px"
           rounded="lg"
           columns={2}
+          bg="orange.500"
+          borderRadius="24px"
         >
           <Box display={'flex'}>
-            <Image src="/images/profile/second.jpg" rounded="lg" />
+            <Image src="/images/abouts/anthropology.jpg" rounded="24px" />
           </Box>
           <Stack justifyContent="space-between" w="100%" p={{ base: 4, md: 2 }}>
-            <Box pt={18} pl={3}>
-              <Text fontSize={20}>
+            <Box pt={18} pl={3} my="auto">
+              <Text size="h3">
                 Enthusiastic junior web developer, keen to build an application
                 for better society and to nurture inspiring and encouraging
                 development team.
               </Text>
             </Box>
-            <Box mr={6} align="right">
+            <Box mr={6} align="right" p={2}>
               <Link href="/images/cv/mihocv.pdf" download="miho.pdf">
-                <Button colorScheme="orange">Get Resume</Button>
+                <Button bg="white" color="orange">
+                  Get Resume
+                </Button>
               </Link>
             </Box>
           </Stack>
         </SimpleGrid>
-        <Box>
-          <Section>
-            <Text fontSize={20}>Highlights:</Text>
-            <List spacing={2} w="100%">
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color="orange" />
-                aaaaaaaaaaaaaaaaaaa
-              </ListItem>
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color="orange" />
-                aaaaaaaaaaaaaaaaaaa
-              </ListItem>
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color="orange" />
-                aaaaaaaaaaaaaaaaaaa
-              </ListItem>
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color="orange" />
-                aaaaaaaaaaaaaaaaaaa
-              </ListItem>
-            </List>
-          </Section>
-          <Section delay={0.2}>
-            <Heading as="h3" variant="section-title">
-              Bio
-            </Heading>
-            <BioSection>
-              <BioYear>1992</BioYear>
-              Born in Tokyo, Japan.
-            </BioSection>
-            <BioSection>
-              <BioYear>1992</BioYear>
-              Born in Tokyo, Japan.
-            </BioSection>
-            <BioSection>
-              <BioYear>1992</BioYear>
-              Born in Tokyo, Japan.
-            </BioSection>
-            <BioSection>
-              <BioYear>1992</BioYear>
-              Born in Tokyo, Japan.
-            </BioSection>
-            <BioSection>
-              <BioYear>1992</BioYear>
-              Born in Tokyo, Japan.
-            </BioSection>
-          </Section>
-          <Section>
-            <Heading as="h3" variant="section-title">
-              Keen on
-            </Heading>
-            <Paragraph>Food, Music, Politics, Whisky, British Comedy</Paragraph>
-          </Section>
+        <Box bg="gray.200" borderRadius="24px" mt={2}>
+          <Box py={6} px={6}>
+            <Section>
+              <Heading as="h3" variant="section-title">
+                Software Development
+              </Heading>
+              <Paragraph>
+                <Text>Food, Music, Politics, Whisky, British Comedy</Text>
+              </Paragraph>
+            </Section>
+            <Section delay={0.2}>
+              <Heading as="h3" variant="section-title">
+                Up for Challenges
+              </Heading>
+              <Paragraph>
+                <Text>Food, Music, Politics, Whisky, British Comedy</Text>
+              </Paragraph>
+            </Section>
+            <Section delay={0.4}>
+              <Heading as="h3" variant="section-title">
+                Towards Humanity
+              </Heading>
+              <Paragraph>
+                <Text>Food, Music, Politics, Whisky, British Comedy</Text>
+              </Paragraph>
+            </Section>
+          </Box>
         </Box>
       </Box>
     </Layout>
