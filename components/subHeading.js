2f22ab43fa2f918a29245873676c03a724e420ea
mihomihouk <mihomihouk@gmail.com>
2022-05-21 10:53:49 +0100

    changed subheading font size

--- components/subHeading.js
+++ components/subHeading.js
@@ -1,9 +1,9 @@
-import { Box, Divider, Heading, Text } from '@chakra-ui/react'
+import { Box, Divider, Heading } from '@chakra-ui/react'
 
 const SubHeading = ({ pageTitle }) => (
-  <Box align="center" mb={6}>
-    <Heading as="h3" variant="page-title" alignItems="center" w="100%">
-      <Text>{pageTitle}</Text>
+  <Box align="center" mb={6} mt={4}>
+    <Heading as="h2" size="3xl" alignItems="center" w="100%">
+      {pageTitle}
     </Heading>
     <Divider borderColor="orange.900" w="250px" />
   </Box>
