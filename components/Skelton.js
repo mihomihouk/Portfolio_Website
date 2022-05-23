f455e7dfc9ce1d949c4a656452599406b9af0acf
mihomihouk <mihomihouk@gmail.com>
2022-05-23 20:33:55 +0100

    added fallback

---
+++ components/Skelton.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Skelton = () => {
+  return (
+    <Box maxWidth="1200px" my="20px" mx="auto">
+      <Box bg="#dbcc1a" borderRadius="4px" my="20px" py="12%"></Box>
+      <Box
+        bg="#dbcc1a"
+        borderRadius="4px"
+        my="20px"
+        py="15px"
+        maxWidth="500px"
+      ></Box>
+      <Box
+        bg="#dbcc1a"
+        borderRadius="4px"
+        my="20px"
+        py="8px"
+        maxWidth="1000px"
+      ></Box>
+      <Box bg="#dbcc1a" borderRadius="4px" my="20px"></Box>
+      <Box bg="#dbcc1a" borderRadius="4px" my="20px"></Box>
+    </Box>
+  )
+}
