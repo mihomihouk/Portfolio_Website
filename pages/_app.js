14c21d213dc849380b6c8cba93742cb4176c0e84
Miho Inagaki <mihomihouk@gmail.com>
2025-11-27 07:29:10 +0000

    Add service to send a log to server

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import theme from '../libs/theme'
 import { AnimatePresence } from 'framer-motion'
+import { useEffect } from 'react'
+import { trackPageView } from '../services/track'
 
 const Website = ({ Component, pageProps, router }) => {
+  useEffect(() => {
+    const pageViewData = {
+      path: router.asPath,
+      userAgent: navigator.userAgent,
+      referrer: document.referrer || undefined
+    }
+    trackPageView(pageViewData)
+  }, [router.asPath])
   return (
     <ChakraProvider theme={theme}>
       <Layout router={router}>
