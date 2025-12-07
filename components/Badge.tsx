c5f1fc467d75ff8ece97267202a0586e3c5cc85d
Miho Inagaki <mihomihouk@gmail.com>
2025-12-07 13:20:22 +0000

    Create Badge component

---
+++ components/Badge.tsx
@@ -0,0 +1,16 @@
+import { Badge as ChakraBadge, BadgeProps as ChakraBadgeProps } from '@chakra-ui/react'
+
+type BadgeProps = {
+    color: ChakraBadgeProps['colorPalette']
+    text: string
+    ml?:number
+    mt?:number
+    mr?:number
+    fontWeight?: 'bold'
+}
+
+export function Badge({color, text, ml, mt, mr, fontWeight}:BadgeProps) {
+  return (
+    <ChakraBadge ml={ml} mt={mt} mr={mr} colorPalette={color} fontWeight={fontWeight}>{text}</ChakraBadge>
+  )
+}
