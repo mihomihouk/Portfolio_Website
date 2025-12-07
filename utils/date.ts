bfd299b02aeb8c0a43a300c1fd1c74a348a000cc
Miho Inagaki <mihomihouk@gmail.com>
2025-12-07 13:24:05 +0000

    Create utils and chart config

---
+++ utils/date.ts
@@ -0,0 +1,14 @@
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import 'dayjs/locale/en-gb'
+import 'dayjs/locale/en'
+import 'dayjs/locale/ja'
+
+dayjs.extend(localizedFormat)
+
+export function formatDate(dateStr: string, format: string): string {
+    const supportedLocales = ['en-gb', 'en', 'ja']
+    const userLocale = navigator.language.toLowerCase()
+    const locale = supportedLocales.includes(userLocale) ? userLocale : 'en-gb'
+    return dayjs(dateStr).locale(locale).format(format)
+}
