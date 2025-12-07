f3c0ab12c1a2857c523eaafef809d2b87eb449dd
Miho Inagaki <mihoinagaki@Mihos-Air.mynet>
2025-12-07 08:25:39 +0000

    Built a service method to get visitor analytics

--- services/analytics.ts
+++ services/analytics.ts
@@ -1,21 +1,46 @@
-type PageViewTrackData = {
+import { config } from "../config"
+
+type PageViewTrackRequestData = {
   path: string
   referrer?: string
   userAgent?: string
 }
 
+type VisitorAnalyticsResponseData = {
+  visitorCount: {
+      visits: number
+      date: string
+  }
+  pagePopularity:{
+      visits: number
+      page: string
+  }
+}
+
+
 export class AnalyticsService {
   constructor() {}
 
-  public static async trackPageView(data: PageViewTrackData) {
+  public static async logPageView(data: PageViewTrackRequestData) {
     try {
-      await fetch('http://localhost:4000/api/log', {
+      await fetch(config.logPageView, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ ...data, event: 'page_view' })
       })
     } catch (error) {
       console.error(error)
+    }
+  }
+
+  public static async getVisitorAnalytics(): Promise<VisitorAnalyticsResponseData>{
+    try{
+      const searchParam = new URLSearchParams({daysAgo: '30'})
+      const requestUrl = `${config.getVisitorAnalytics}?${searchParam}`
+      return (await fetch(requestUrl)).json() 
+    }catch(err){
+      console.error(err)
+      throw new Error('Failed to get visitor analytics')
     }
   }
 }
