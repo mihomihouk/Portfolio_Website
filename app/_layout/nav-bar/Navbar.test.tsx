8ba4b9c721ef4258fe760c77cbb001e56c05a2b1
Miho Inagaki <mihomihouk@gmail.com>
2025-12-08 17:49:24 +0000

    Add About page tests

--- app/_layout/nav-bar/Navbar.test.tsx
+++ app/_layout/nav-bar/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test, expect, vi } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { fireEvent, screen, waitFor, within } from '@testing-library/react'
 import { Navbar } from './Navbar'
 import { renderWithProviders } from '../../../tests/utils/test-utils'
